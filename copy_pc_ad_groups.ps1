1b77b21ad426cf2ea7e56b8372fa3c506a408fa5
Robbie <Robbie_Vos@outlook.com>
2025-07-09 14:50:39 +0200

    Update and rename get_pc_ad_groups.ps1 to copy_pc_ad_groups.ps1

---
+++ copy_pc_ad_groups.ps1
@@ -0,0 +1,17 @@
+# Vraag de oude en nieuwe computernaam op
+$OldComputerName = Read-Host "Voer de oude computernaam in: "
+$NewComputerName = Read-Host "Voer de nieuwe computernaam in: "
+
+# Haal de AD computerobjecten op
+$OldComputer = Get-ADComputer -Identity $OldComputerName -Properties MemberOf
+$NewComputer = Get-ADComputer -Identity $NewComputerName
+
+if ($OldComputer -and $NewComputer) {
+    # Doorloop de groepen van de oude computer en voeg de nieuwe toe
+    foreach ($GroupDN in $OldComputer.MemberOf) {
+        Add-ADGroupMember -Identity $GroupDN -Members $NewComputer
+        Write-Host "Toegevoegd aan groep: $GroupDN"
+    }
+    Write-Host "Alle groepen zijn succesvol gekopieerd."
+} else {
+    Write-Host "Een van de computerobjecten kon niet gevonden worden. Controleer de ingevoerde namen."
--- get_pc_ad_groups.ps1
+++
@@ -1,5 +0,0 @@
-$ComputerName = Read-Host "Voer computer naam in: "
-
-Get-ADComputer $ComputerName -Property MemberOf | Select-Object -ExpandProperty MemberOf | ForEach-Object {
-    ($_ -split ',')[0] -replace 'CN='
-}
