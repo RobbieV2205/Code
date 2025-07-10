43ca261d3961f24568a4a7c234da19aef65c43b9
Robbie <Robbie_Vos@outlook.com>
2025-07-10 09:58:19 +0200

    Update get_pc_ad_groups.ps1

--- get_pc_ad_groups.ps1
+++ get_pc_ad_groups.ps1
@@ -1,1 +1,5 @@
-#content
+$ComputerName = Read-Host "Voer computer naam in: "
+
+Get-ADComputer $ComputerName -Property MemberOf | Select-Object -ExpandProperty MemberOf | ForEach-Object {
+    ($_ -split ',')[0] -replace 'CN='
+}
